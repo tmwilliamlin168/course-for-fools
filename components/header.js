669466bf81093e0711b75193366ba60656d1b481
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-04-01 02:06:14 +0800

    header offset

--- components/header.js
+++ components/header.js
@@ -13,36 +13,39 @@ const internalLinks = [
 
 export default function Header() {
     return (
-        <div className="bg-white shadow fixed inset-x-0 top-0 z-10">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-          <div className="flex">
-            <Link
-              href="/"
-              className="flex-shrink-0 flex items-center text-xl font-bold"
-            >
-              Logo
-            </Link>
-            <div className="hidden sm:ml-8 sm:flex space-x-8">
-              <a
-                href="https://course.williamlin.io/"
-                target="_blank"
-                className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
-              >
-                Course
-              </a>
-              {internalLinks.map(link => (
+        <>
+            <div className="bg-white shadow fixed inset-x-0 top-0 z-10">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+            <div className="flex">
                 <Link
-                  href={link.url}
-                  className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                href="/"
+                className="flex-shrink-0 flex items-center text-xl font-bold"
                 >
-                  {link.label}
+                Logo
                 </Link>
-              ))}
+                <div className="hidden sm:ml-8 sm:flex space-x-8">
+                <a
+                    href="https://course.williamlin.io/"
+                    target="_blank"
+                    className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                >
+                    Course
+                </a>
+                {internalLinks.map(link => (
+                    <Link
+                    href={link.url}
+                    className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                    >
+                    {link.label}
+                    </Link>
+                ))}
+                </div>
             </div>
-          </div>
+            </div>
         </div>
-      </div>
-        </div>
+            </div>
+            <div className="m-16"></div>
+        </>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ export default function Home() {
       </Head>
       <div>
         <Header />
+        hi
       </div>
     </>
   )
