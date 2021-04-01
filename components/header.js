3dfdb86f0ad13f06600f474ab3b7bf3fda0fe09c
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-04-01 17:27:42 +0800

    favicon

--- components/header.js
+++ components/header.js
@@ -37,7 +37,7 @@ export default function Header() {
                     link.url ? 
                         <Link
                             href={link.url}
-                            key={link.url}
+                            key={link.label}
                         >
                             <a className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                                 {link.label}
@@ -45,6 +45,7 @@ export default function Header() {
                         </Link>
                     :
                         <a
+                            key={link.label}
                             className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
                             title="Coming soon!"
                         >
