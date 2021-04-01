3243a45d13121331d2fc9a8a72e1a3f63b296b68
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-04-01 09:42:47 +0800

    test intro & rules

--- pages/register.js
+++ pages/register.js
@@ -50,17 +50,39 @@ export default function Register() {
         })();
     }, [user]);
 
+    const renderTest = testData => {
+        if(testData.stage === 0) {
+            return (
+                <>
+                    To pre-register for this course, some very basic math and logic skills will be required. An assessment will be given to determine if you are eligible for the beta version of this course, as well as help you identify any skills that should be improved (if any).
+                    <br />
+                    1. You have 30 minutes to complete this assessment.
+                    <br />
+                    2. You will need a score of 60% or above to pass.
+                    <br />
+                    3. You will need to submit the solution to each problem separately. 
+                    <br />
+                    <button
+                        onClick={() => {
+
+                        }}
+                    >
+                        Start Test
+                    </button>
+                </>
+            )
+        }
+    };
+
     return (
         <>
             <h1>Test</h1>
-            <p>
-                {
-                    !testData?
-                        'Loading'
-                    :
-                        JSON.stringify(testData)
-                }
-            </p>
+            {
+                !testData?
+                    'Loading'
+                :
+                    renderTest(testData)
+            }
         </>
     )
 }
