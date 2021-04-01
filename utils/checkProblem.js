0f965087ba4af2051be9c4785b3bdb8742cd348c
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-04-01 14:27:26 +0800

    Update checkProblem.js
    remove console output

--- utils/checkProblem.js
+++ utils/checkProblem.js
@@ -11,11 +11,9 @@ const problemCheckers = [
     },
     (oldData, action) => {
         const l=action.split('');
-        console.log(l);
         if(l.length !== 25 || l.some(c => c !== '0' && c !== '1'))
             return {wa: 1};
         let cnt=0;
-        console.log('hi2');
         for(let i=0; i<5; ++i) {
             for(let j=0; j<5; ++j) {
                 if(!(l[i*5+j] === '1' || i > 0 && l[(i-1)*5+j] === '1' || i < 4 && l[(i+1)*5+j] === '1' || j > 0 && l[i*5+(j-1)] === '1' || j < 4 && l[i*5+(j+1)] === '1'))
