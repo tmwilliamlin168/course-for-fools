84bfbd46bc53bd95a9022efdad6bbff57959d605
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-04-01 18:24:24 +0800

    more landing page

--- components/header.js
+++ components/header.js
@@ -26,11 +26,10 @@ export default function Header() {
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
             <div className="flex">
-                <Link
-                href="/"
-                className="flex-shrink-0 flex items-center text-xl font-bold"
-                >
-                Course Name!
+                <Link href="/">
+                    <a className="bg-clip-text text-transparent flex-shrink-0 inline-flex items-center text-xl font-bold bg-gradient-to-r from-blue-300 to-blue-600">
+                        Zero to Hero
+                    </a>
                 </Link>
                 <div className="hidden sm:ml-8 sm:flex space-x-8">
                 {internalLinks.map(link => (
@@ -39,14 +38,14 @@ export default function Header() {
                             href={link.url}
                             key={link.label}
                         >
-                            <a className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
+                            <a className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                                 {link.label}
                             </a>
                         </Link>
                     :
                         <a
                             key={link.label}
-                            className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                            className="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
                             title="Coming soon!"
                         >
                             {link.label}
--- pages/index.js
+++ pages/index.js
@@ -56,46 +56,172 @@ export default function Home() {
         <title>Course</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
-        <div className="mx-auto max-w-7xl lg:px-8">
-          <div className="lg:grid lg:grid-cols-8 lg:gap-8">
-            <div className="mx-auto lg:ml-0 max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center lg:col-span-5">
-              <div className="lg:py-24">
-                <h1 className="text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-                  <span className="block">Competitive Programming:</span>
-                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-blue-600 block">
-                    Zero to Hero
-                  </span>
-                </h1>
-                <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                  Learn Competitive Programming with no previous experience required.
-                  <br />
-                  Taught by the winner of Google Kickstart and IOI.
-                </p>
-                <div className="mt-8 sm:mt-12 space-x-4">
-                  <button
-                    className="inline-flex sm:text-lg py-3 px-6 sm:py-4 sm:px-8 rounded-md shadow bg-blue-500 text-white font-medium hover:bg-blue-700 transition focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
-                    onClick={() => firebaseLogin()}
-                  >
-                    Pre-Register Now
-                  </button>
-                  <a
-                    href="mailto:ytmwilliamlin@gmail.com"
-                    target="_blank"
-                    className="inline-flex sm:text-lg py-3 px-6 sm:py-4 sm:px-8 rounded-md shadow bg-gray-800 text-white font-medium hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 focus:ring-offset-gray-900 transition"
-                  >
-                    Contact Us
-                  </a>
+      <div className="min-h-screen">
+        <Header />
+        <div className="pt-10 bg-gray-900 sm:pt-16 lg:pt-8 lg:pb-14 lg:overflow-hidden">
+          <div className="mx-auto max-w-7xl lg:px-8">
+            <div className="lg:grid lg:grid-cols-8 lg:gap-8">
+              <div className="mx-auto lg:ml-0 max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center lg:col-span-5">
+                <div className="lg:py-24">
+                  <h1 className="text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                    <span className="block">Competitive Programming:</span>
+                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-blue-600 block">
+                      Zero to Hero
+                    </span>
+                  </h1>
+                  <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                    Learn Competitive Programming with no previous experience required.
+                    <br />
+                    Taught by the winner of Google Kickstart and IOI.
+                  </p>
+                  <div className="mt-8 sm:mt-12 space-x-4">
+                    <button
+                      className="inline-flex sm:text-lg py-3 px-6 sm:py-4 sm:px-8 rounded-md shadow bg-blue-500 text-white font-medium hover:bg-blue-700 transition focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
+                      onClick={() => firebaseLogin()}
+                    >
+                      Pre-Register Now
+                    </button>
+                    <a
+                      href="mailto:ytmwilliamlin@gmail.com"
+                      target="_blank"
+                      className="inline-flex sm:text-lg py-3 px-6 sm:py-4 sm:px-8 rounded-md shadow bg-gray-800 text-white font-medium hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 focus:ring-offset-gray-900 transition"
+                    >
+                      Contact Us
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-12 -mb-16 sm:-mb-48 lg:mt-10 lg:-mb-16 lg:relative lg:col-span-3">
+                <div className="mx-auto max-w-md px-4 sm:max-w-xl sm:px-6 lg:max-w-none lg:px-0">
+                  <img
+                    className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                    src="/courseff.png"
+                  />
                 </div>
               </div>
             </div>
-            <div className="mt-12 -mb-16 sm:-mb-48 lg:mt-10 lg:-mb-16 lg:relative lg:col-span-3">
-              <div className="mx-auto max-w-md px-4 sm:max-w-xl sm:px-6 lg:max-w-none lg:px-0">
-                <img
-                  className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                  src="/courseff.png"
-                />
+          </div>
+        </div>
+
+        <div class="relative bg-white py-16 sm:py-24 lg:py-32">
+          <div class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+            <h2 class="text-base font-semibold tracking-wider text-blue-500 uppercase">Learn Smarter, not Harder</h2>
+            <p class="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+              Everything you need to become a CP master
+            </p>
+            <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+              We cover everything you will ever need to know. All in one packaged, easy-to-digest course.
+            </p>
+            <div class="mt-12">
+              <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                <div class="pt-6">
+                  <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                    <div class="-mt-6">
+                      <div>
+                        <span class="inline-flex items-center justify-center p-3 bg-gradient-to-r from-blue-400 to-blue-600 rounded-md shadow-lg">
+                          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                          </svg>
+                        </span>
+                      </div>
+                      <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">Clear Roadmap</h3>
+                      <p class="mt-5 text-base text-gray-500">
+                        Our roadmap provides instruction from newbie to grandmaster - you will never feel lost along the way.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="pt-6">
+                  <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                    <div class="-mt-6">
+                      <div>
+                        <span class="inline-flex items-center justify-center p-3 bg-gradient-to-r from-blue-400 to-blue-600 rounded-md shadow-lg">
+                          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                          </svg>
+                        </span>
+                      </div>
+                      <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">Experienced Instructors</h3>
+                      <p class="mt-5 text-base text-gray-500">
+                        Above 90% of our instructors have either won Google Kickstart rounds or gotten gold medals at IOI.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="pt-6">
+                  <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                    <div class="-mt-6">
+                      <div>
+                        <span class="inline-flex items-center justify-center p-3 bg-gradient-to-r from-blue-400 to-blue-600 rounded-md shadow-lg">
+                          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z" />
+                          </svg>
+                        </span>
+                      </div>
+                      <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">Detailed Explanations</h3>
+                      <p class="mt-5 text-base text-gray-500">
+                        Through a combination of diagrams, videos, and interactive visualizers, you will learn the toughest concepts with ease.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="pt-6">
+                  <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                    <div class="-mt-6">
+                      <div>
+                        <span class="inline-flex items-center justify-center p-3 bg-gradient-to-r from-blue-400 to-blue-600 rounded-md shadow-lg">
+                          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                          </svg>
+                        </span>
+                      </div>
+                      <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">Thinking Strategies</h3>
+                      <p class="mt-5 text-base text-gray-500">
+                        Hand-crafted thinking exercises to help you approach problems mathematically and logically.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="pt-6">
+                  <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                    <div class="-mt-6">
+                      <div>
+                        <span class="inline-flex items-center justify-center p-3 bg-gradient-to-r from-blue-400 to-blue-600 rounded-md shadow-lg">
+                          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+                          </svg>
+                        </span>
+                      </div>
+                      <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">Professional Help</h3>
+                      <p class="mt-5 text-base text-gray-500">
+                        No more fear of being downvoted for asking questions - our instructors are here to support you, no matter what you need.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="pt-6">
+                  <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+                    <div class="-mt-6">
+                      <div>
+                        <span class="inline-flex items-center justify-center p-3 bg-gradient-to-r from-blue-400 to-blue-600 rounded-md shadow-lg">
+                          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                          </svg>
+                        </span>
+                      </div>
+                      <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">Contest Strategies</h3>
+                      <p class="mt-5 text-base text-gray-500">
+                        Tips and tricks from years worth of experience in contests for you to optimize contest rankings.
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
