268b975c168d26092e53127d23664dbf9a1a8a90
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-04-01 09:22:15 +0800

    putting stuff in db

--- pages/api/teststatus.js
+++ pages/api/teststatus.js
@@ -1,12 +1,26 @@
 import {database} from '../../utils/firebase'
 import verifyToken from '../../utils/verifyToken';
 
+const usersRef=database.ref('users');
+
 export default async (req, res) => {
     const ui = await verifyToken(req.query.token);
     if(!ui) {
         res.status(400).end();
         return;
     }
+
     console.log(ui);
-    res.status(200).json({stage:0});
+    const uid=ui.uid;
+    const userRef=usersRef.child(uid);
+    let user=await userRef.once('value');
+    if (!user.exists()) {
+		//create new
+        await userRef.set({name: ui.name, email: ui.email, testData: {stage: 0}});
+        user=await userRef.once('value');
+	} else {
+        
+    }
+
+    res.status(200).json(user.testData);
 }
