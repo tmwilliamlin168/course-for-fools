2f794836fb31888669ea03a3fea0a2f2c2914a8e
Cynthia Du <47993494+cynthiakedu@users.noreply.github.com>
2021-04-02 00:23:34 -0400

    Add result page

---
+++ pages/result.js
@@ -0,0 +1,49 @@
+
+export default function Register() {
+    return (
+        <>
+                    <div className="flex mt-20 justify-center h-screen">
+            <div className=" ">
+              <div className="max-w-2xl h-100 bg-gray-200 rounded overflow-hidden shadow-lg">
+                <div className="px-6 py-6">
+
+                    <p className="text-gray-700 text-base mx-7">
+                    Dear Name,
+                    </p>
+                    <br/>
+                    <p className="text-gray-700 text-base mx-7">
+                    We want to thank you for the time and effort you took to submit your application to Zero Hero. 
+                    <br/>
+                    <br/>
+                    </p>
+                        <p className="text-gray-700 text-base mx-7">
+                           The course was extremely popular (because tmw is too op) and there was a large number 
+                            of strong participants applying. After careful consideration,
+                            we regret to inform you that you were not selected. 
+                        </p>
+                    <br/>
+
+                    <p className="text-gray-700 text-base mx-7">
+                        You can continue to follow tmw's updates:
+                    </p>
+                    <div className="ml-12 mt-3">
+                        <a href="https://twitter.com/tmwilliamlin168?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow @tmwilliamlin168</a>
+                        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                    </div>
+                    <br/>
+                    <p className="text-gray-700 text-base mx-7">
+                        We encourage you to continue practicing competitive programming and wish
+                        you the best of luck at beating tmw.
+                    </p>
+                    <br/>
+
+                     <p className="text-gray-700 text-base mx-7">
+                        Zero Hero
+                    </p>
+                    </div>
+                </div> 
+              </div>
+            </div>
+        </>
+    )
+}
